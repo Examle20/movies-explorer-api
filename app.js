5e7a38c48a9159475f94843c488e2c8a72ae2d64
engineer <Examle20@yandex.ru>
2021-06-03 17:22:13 +0300

    feat: подключить helmet

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 const handleError = require('./middlewares/handleErorr');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -9,6 +10,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
