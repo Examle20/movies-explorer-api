743f79e75971239ddedfe9f703f782eb75fb4ad6
engineer <Examle20@yandex.ru>
2021-06-04 00:47:12 +0300

    feat: обработать ошибку 404

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const movies = require('./movies');
 const auth = require('../middlewares/auth');
 const { createUser, login } = require('../controllers/users');
 const { signupValidation, signinValidation } = require('../middlewares/bodyValidation');
-const NotFoundError = require('../errors/NotFoundError')
-const {NOT_FOUND_ERROR} = require('../utils/constans')
+const NotFoundError = require('../errors/NotFoundError');
+const { NOT_FOUND_ERROR } = require('../utils/constans');
 
 router.post('/signup', signupValidation, createUser);
 router.post('/signin', signinValidation, login);
@@ -14,6 +14,6 @@ router.use(auth, users);
 router.use(auth, movies);
 
 router.use((req, res, next) => {
-  next(new NotFoundError(NOT_FOUND_ERROR))
-})
+  next(new NotFoundError(NOT_FOUND_ERROR));
+});
 module.exports = router;
